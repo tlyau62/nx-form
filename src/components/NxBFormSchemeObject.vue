a9d52856a933694ee45f03137866664418b425b9
Tsz Lam <tyautl@nexify.com.hk>
2021-05-13 20:27:14 +0800

    Add object cloning

--- src/components/NxBFormSchemeObject.vue
+++ src/components/NxBFormSchemeObject.vue
@@ -12,7 +12,18 @@
 </template>
 
 <script>
-import { compose, curry, T, eq, prop, cond, constant } from "lodash/fp";
+import {
+  compose,
+  curry,
+  T,
+  eq,
+  prop,
+  cond,
+  constant,
+  mapValues,
+  isPlainObject,
+  clone,
+} from "lodash/fp";
 
 export default {
   props: {
@@ -22,15 +33,38 @@ export default {
   },
   data() {
     return {
-      localValue: this.value,
+      localValue: null,
+      cacheValue: null,
     };
   },
   computed: {
+    localValueWatch() {
+      return isPlainObject(this.localValue)
+        ? Object.values(this.localValue)
+        : this.localValue;
+    },
     fields() {
       return Object.entries(this.schema.fields).map((entry) => [
         ...entry,
         mapTypeToComponent(entry[1]),
       ]);
+    },
+  },
+  watch: {
+    localValueWatch: {
+      handler() {
+        this.cacheValue = clone(this.localValue);
+
+        this.$emit("input", this.cacheValue);
+      },
+    },
+    value: {
+      handler(value) {
+        if (value !== this.cacheValue) {
+          this.localValue = createSchemaModel(this.schema);
+        }
+      },
+      immediate: true,
     },
   },
   beforeCreate() {
@@ -50,4 +84,34 @@ export const mapTypeToComponent = cond([
   [equalType("object"), constant("NxBFormSchemaObjectField")],
   [T, constant("NxBFormSchemaTextField")],
 ]);
+
+export const createSchemaModel = cond([
+  [
+    equalType("object"),
+    (scheme) => createSchemaModelOnObjectType(scheme.fields),
+  ],
+  [T, constant(undefined)],
+]);
+
+export const createSchemaModelOnObjectType = mapValues((v) =>
+  createSchemaModel(v)
+);
+
+// export const createModelFromSchemeWithValue = (scheme, value) =>
+//   projectDeep(createSchemaModel(scheme), value);
+
+// export const project = (target, source) =>
+//   mapValues(target, (v, k) => v ?? source[k]);
+
+// export const projectDeep = (target, source) =>
+//   mapValues(target, (v, k) => {
+//     if (v) {
+//       if (isPlainObject(v)) {
+//         return projectDeep(v, source[k] || {});
+//       } else {
+//         return v;
+//       }
+//     }
+//     return source[k];
+//   });
 </script>
