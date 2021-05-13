75fe9539293b7d36e85f27091b9582f60bb86ba0
Tsz Lam <tyautl@nexify.com.hk>
2021-05-13 19:34:33 +0800

    Remove junk func

--- src/components/nx-b-form-schema-fields.js
+++ src/components/nx-b-form-schema-fields.js
@@ -1,6 +1,5 @@
 import { createSchemaField } from "../utils";
 import { BFormGroup } from "bootstrap-vue";
-import { cond, curry, prop, compose, eq, constant, T } from "lodash/fp";
 import NxBFormSchemaInput from "./NxBFormSchemaInput";
 import NxBFormSchemeObject from "./NxBFormSchemeObject";
 
@@ -11,11 +10,3 @@ export const NxBFormSchemaTextField =
 
 export const NxBFormSchemaObjectField =
   createSchemaFieldWitBFormGroup(NxBFormSchemeObject);
-
-export const equalType = curry((type, value) =>
-  compose(eq(type), prop("type"))(value)
-);
-
-export const getSchemaFieldByType = cond([
-  [T, constant(NxBFormSchemaTextField)],
-]);
