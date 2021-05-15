97965001d05852b84e1d21f31958bb291415de91
Tsz Lam <tyautl@nexify.com.hk>
2021-05-15 18:51:25 +0800

    Add required

--- src/utils/nx-json-schema-parser.js
+++ src/utils/nx-json-schema-parser.js
@@ -1,12 +1,21 @@
-import { mapValues, omit, omitBy, isArray } from "lodash";
+import {
+  mapValues,
+  omit,
+  omitBy,
+  isArray,
+  capitalize,
+  lowerCase,
+} from "lodash";
 
 export const visitor = () => ({
-  prop(prop) {
+  prop(prop, propName) {
     this.setValue(
       {
         type: isArray(prop.type) ? prop.type[0] : prop.type,
-        label: prop.title,
+        label: prop.title || capitalize(lowerCase(propName)),
         fields: prop.properties,
+        format: prop.format,
+        required: isArray(prop.type) && prop.type.includes("null"),
       },
       "fields"
     );
@@ -14,10 +23,11 @@ export const visitor = () => ({
 });
 
 export class JsonSchemaModel {
-  constructor(value) {
+  constructor(value, name = "root") {
     this._value = null;
     this._propKey = "properties";
     this._children = null;
+    this._name = name;
 
     this.setValue(value, "properties");
   }
@@ -29,7 +39,10 @@ export class JsonSchemaModel {
   setValue(value, propKey) {
     this._value = omitBy(value, (value) => value === undefined);
     this.setChildren(
-      mapValues(value[propKey] || {}, (prop) => new JsonSchemaModel(prop))
+      mapValues(
+        value[propKey] || {},
+        (prop, name) => new JsonSchemaModel(prop, name)
+      )
     );
     this._value = omit(value, propKey);
     this.setPropKey(propKey);
@@ -55,6 +68,14 @@ export class JsonSchemaModel {
     return Object.keys(this._children).length > 0;
   }
 
+  getName() {
+    return this._name;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
   export() {
     return {
       ...this.getValue(),
@@ -66,7 +87,7 @@ export class JsonSchemaModel {
 }
 
 export const traverse = (model, visitor) => {
-  visitor.prop.call(model, model.export());
+  visitor.prop.apply(model, [model.export(), model.getName()]);
 
   for (const child of Object.values(model.getChildren())) {
     traverse(child, visitor);
