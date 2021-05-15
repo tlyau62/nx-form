21fc4e79701ae2fcb459ee19cbd3cd547e3c2848
Tsz Lam <tyautl@nexify.com.hk>
2021-05-15 18:52:38 +0800

    Add json schema

--- src/views/FormJsonSchema.vue
+++ src/views/FormJsonSchema.vue
@@ -5,10 +5,24 @@
         <b-form @submit.prevent="handleSubmit(submit($event))">
           <h5>Form design 3</h5>
 
+          <h6>Test form 1</h6>
+
+          {{ form }}
+
           <NxBFormJsonSchemaField
             name="root"
             :schema="jsonSchema"
             v-model="form"
+          />
+
+          <h6>Test form 2</h6>
+
+          {{ form2 }}
+
+          <NxBFormJsonSchemaField
+            name="root"
+            :schema="jsonSchema2"
+            v-model="form2"
           />
 
           <b-button type="submit">Submit</b-button>
@@ -41,6 +55,23 @@ const jsonSchema = {
   },
 };
 
+const jsonSchema2 = {
+  title: "MinuteWorkflowForm",
+  type: "object",
+  properties: {
+    id: { type: ["string", "null"] },
+    commentForUpdate: { type: ["string", "null"] },
+    dueDateForUpdate: { type: ["string", "null"], format: "date-time" },
+    minuteID: { type: "integer" },
+    senderUserID: { type: "integer" },
+    state: { type: ["string", "null"] },
+    subMinuteID: { type: ["integer", "null"] },
+    submissionID: { type: "integer" },
+    submissionJson: { type: ["string", "null"] },
+    taskType: { type: ["string", "null"] },
+  },
+};
+
 export default {
   components: {
     ValidationObserver,
@@ -49,7 +80,9 @@ export default {
   data() {
     return {
       jsonSchema,
+      jsonSchema2,
       form: {},
+      form2: {},
     };
   },
   methods: {
