2ad47749b7d23afe57fda55011246f2f40209421
Tsz Lam <tyautl@nexify.com.hk>
2021-05-18 17:09:47 +0800

    Test on v-model object value

--- tests/unit/components/nx-b-form-schema-fields.spec.js
+++ tests/unit/components/nx-b-form-schema-fields.spec.js
@@ -177,5 +177,73 @@ describe("nx-b-form-schema-fields.js", () => {
       expect(isPlainObject(wrapper.vm.form)).toBe(true);
       expect(wrapper.vm.form).toEqual(cloneDeep(defaultValue));
     });
+
+    it("does not modify any object in v-model", async () => {
+      const schema = {
+        type: "object",
+        fields: {
+          person: {
+            type: "string",
+          },
+          loveColor: {
+            type: "object",
+            fields: {
+              color1: {
+                type: "string",
+              },
+              color2: {
+                type: "object",
+                fields: {
+                  color2A: {
+                    type: "string",
+                  },
+                  color2B: {
+                    type: "string",
+                  },
+                },
+              },
+            },
+          },
+        },
+      };
+
+      const defaultValue = {
+        person: "peter",
+        loveColor: {
+          color1: "black",
+          color2: {
+            color2A: "black",
+            color2B: "blue",
+          },
+        },
+      };
+
+      // make sure formValue is not modified
+      const formValue = cloneDeep(defaultValue);
+
+      const wrapper = mount(createWrapper("color1", schema, formValue));
+
+      await wrapper.vm.$nextTick();
+
+      expect(wrapper.vm.form).not.toBe(formValue);
+      expect(wrapper.vm.form).toEqual(formValue);
+
+      // make sure color2 is not clone, but loveColor is cloned
+      let field = null;
+      let person = wrapper.vm.form;
+      let clonePerson = cloneDeep(person);
+
+      field = wrapper.find(`input[name='color1']`);
+
+      field.element.value = `dark blue`;
+      field.trigger("input");
+
+      await wrapper.vm.$nextTick();
+
+      expect(wrapper.vm.form).not.toBe(person);
+      expect(wrapper.vm.form.loveColor).not.toBe(person.loveColor);
+      expect(wrapper.vm.form.loveColor.color2).toBe(person.loveColor.color2);
+      expect(person).toEqual(clonePerson);
+    });
   });
 });
